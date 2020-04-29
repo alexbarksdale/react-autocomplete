a15a77a85ce6c929dc74b7a5aa2024aea6136b7c
alexbarksdale <ale.barksdale@gmail.com>
2020-04-29 14:04:46 -0500

    [Add] allTreeStrings method

--- src/assets/prefixtree.ts
+++ src/assets/prefixtree.ts
@@ -29,6 +29,48 @@ export class PrefixTree {
         }
     }
 
+    private findNode(str: string): [PrefixTreeNode, number] {
+        /* Return a pair containing the deepest node in this prefix tree that
+        matches the longest prefix of the given string and the node's depth.
+        The depth returned is equal to the number of prefix characters matched.
+        Search is done iteratively with a loop starting from the root node. */
+
+        // Match the empty string
+        if (str.length === 0) {
+            return [this.root, 0];
+        }
+
+        // Start with the root node and count depth
+        let [node, depth] = [this.root, 0];
+        for (const char of str) {
+            if (node.hasChild(char)) {
+                node = node.getChild(char); // Found the char, go next
+                depth += 1;
+            }
+        }
+        return [node, depth];
+    }
+
+    private traverse(
+        node: PrefixTreeNode,
+        prefix: string,
+        visit: (c: string) => void
+    ): void {
+        /* Traverse this prefix tree with recursive depth-first traversal.
+        Start at the given node with the given prefix representing its path in
+        this prefix tree and visit each node with the given visit function. */
+
+        // Once the node is filled with characters and contains a terminal node, it'll append
+        if (node.isTerminal()) {
+            visit(prefix);
+        }
+
+        for (const char of node.children.keys()) {
+            const next_node = node.getChild(char);
+            this.traverse(next_node, prefix + char, visit);
+        }
+    }
+
     isEmpty(): number {
         /* Return True if this prefix tree is empty (contains no strings). */
         return this.size;
@@ -49,6 +91,7 @@ export class PrefixTree {
     }
 
     insert(str: string): void {
+        /* Insert the given string into this prefix tree. */
         let node = this.root;
 
         for (const char of str) {
@@ -66,24 +109,10 @@ export class PrefixTree {
         }
     }
 
-    private findNode(str: string): [PrefixTreeNode, number] {
-        // Match the empty string
-        if (str.length === 0) {
-            return [this.root, 0];
-        }
-
-        // Start with the root node and count depth
-        let [node, depth] = [this.root, 0];
-        for (const char of str) {
-            if (node.hasChild(char)) {
-                node = node.getChild(char); // Found the char, go next
-                depth += 1;
-            }
-        }
-        return [node, depth];
-    }
-
     complete(prefix: string): string[] {
+        /* Return a list of all strings stored in this prefix tree that start
+           with the given prefix string. */
+
         // Create a list of completions in prefix tree
         const completions: string[] = [];
 
@@ -100,18 +129,12 @@ export class PrefixTree {
         return completions;
     }
 
-    private traverse(
-        node: PrefixTreeNode,
-        prefix: string,
-        visit: (c: string) => void
-    ): void {
-        if (node.isTerminal()) {
-            visit(prefix);
-        }
+    allTreeStrings(): string[] {
+        /* Return a list of all strings stored in this prefix tree. */
 
-        for (const char of node.children.keys()) {
-            const next_node = node.getChild(char);
-            this.traverse(next_node, prefix + char, visit);
-        }
+        // Create a list of all strings in prefix tree
+        const all_strings: string[] = [];
+        this.traverse(this.root, '', all_strings.push);
+        return all_strings;
     }
 }
