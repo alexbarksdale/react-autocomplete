0b62b0c1b677d7c1a9763ce1bcdaa946134f9464
alexbarksdale <ale.barksdale@gmail.com>
2020-05-02 03:12:52 -0500

    [Add] Result dropdown

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,39 +1,89 @@
 import React, { useState, useEffect, Dispatch, ChangeEvent, FormEvent } from 'react';
 import styled from 'styled-components';
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaChevronRight } from 'react-icons/fa';
 
 import { PrefixTree } from '../assets/prefixtree';
 
 // **** SEARCH BAR STYLES ****
 const SearchContainer = styled.div`
     display: flex;
-    align-items: center;
     border-radius: 8px;
     background-color: #fff;
-    width: 286px;
-    padding: 5px;
+
+    form {
+        width: 286px;
+    }
 
     span {
-        margin-left: 14px;
+        margin: 14px 0px 14px 14px;
+        position: absolute;
     }
 `;
 
 const SearchInput = styled.input`
-    border: none;
-    padding: 13px;
-    border-radius: 8px;
-    width: 100%;
+    color: #3d3d3d;
     font-size: 16px;
+    width: -webkit-fill-available;
+    margin-left: 30px;
+    padding: 13px;
+    border: none;
+    border-radius: 8px;
+    box-sizing: border-box;
     &:focus {
         outline: none;
     }
     &::placeholder {
+        color: #878787;
         font-size: 16px;
     }
 `;
+
+const SearchIcon = styled(FaSearch)`
+    color: #3d3d3d;
+`;
+
+const SearchResultIcon = styled(FaChevronRight)`
+    color: #3d3d3d;
+    margin-right: 14px;
+`;
+
+const SearchListContainer = styled.ul`
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    padding: 0px 12px 12px 12px;
+    form {
+        width: 285px;
+    }
+
+    li {
+        margin-top: 9px;
+
+        a {
+            color: #3d3d3d;
+            display: flex;
+            align-items: center;
+            width: auto;
+            padding: 7px;
+            border-radius: 8px;
+            text-decoration: none;
+            cursor: pointer;
+
+            &:hover {
+                transition: all 0.3s ease-in-out;
+                background-color: #eaeaea;
+            }
+        }
+    }
+`;
+
+const ListingDivider = styled.div`
+    height: 2px;
+    background-color: #eaeaea;
+`;
 // **** END SEARCH BAR STYLES ****
 
-/* [TODOS]
+/*
 Functionality to add:
 TODO: Customizable placeholder
 TODO: Load corpus
@@ -42,6 +92,7 @@ TODO: Add type change Ex: text, password, etc
 TODO: Disable searchicon
 TODO: Add exact match option if possible?
 TODO: Convert to CSS
+TODO: Color matched characters differently
 */
 
 interface AppState {
@@ -70,6 +121,20 @@ const handleTermChange = (
     setSearch({ searchTree, searchTerm: e.target.value });
 };
 
+const displayResults = (searchResults: string[]): JSX.Element[] => {
+    const resultListElement = searchResults.map((result) => {
+        return (
+            <li>
+                <a href='REPLACE_ME'>
+                    <SearchResultIcon />
+                    {result}
+                </a>
+            </li>
+        );
+    });
+    return resultListElement;
+};
+
 export function SearchBar(): JSX.Element {
     const [search, setSearch] = useState<AppState>({
         searchTree: new PrefixTree(),
@@ -80,7 +145,7 @@ export function SearchBar(): JSX.Element {
     const { searchTree, searchTerm } = search;
 
     // Retrieves all the search term completions
-    const searchResults = searchTree.complete(searchTerm);
+    const searchResults: string[] = searchTree.complete(searchTerm);
 
     // PLAYGROUND - remove later
     console.log('TREE:', search.searchTree);
@@ -106,7 +171,7 @@ export function SearchBar(): JSX.Element {
     return (
         <SearchContainer>
             <span>
-                <FaSearch />
+                <SearchIcon />
             </span>
             <form onSubmit={(e) => handleTermSubmit(e, search, setSearch)}>
                 <SearchInput
@@ -114,9 +179,15 @@ export function SearchBar(): JSX.Element {
                     value={searchTerm}
                     onChange={(e) => handleTermChange(e, searchTree, setSearch)}
                 />
+                {searchResults.length > 0 ? (
+                    <>
+                        <ListingDivider />
+                        <SearchListContainer>
+                            {displayResults(searchResults)}
+                        </SearchListContainer>
+                    </>
+                ) : null}
             </form>
-
-            <h1>{search.searchTerm}</h1>
         </SearchContainer>
     );
 }
